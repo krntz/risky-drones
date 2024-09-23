64502a355ab489ac565c8d9fc19e9355c543c6b3
Amandus Krantz <me@amanduskrantz.com>
2024-09-23 11:46:53 +0200

    linting

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import random
 import statistics
 import sys
 import time
-from operator import truediv
 from pathlib import Path
 
 from flask import Flask, render_template
@@ -26,7 +25,7 @@ sock = Sock(app)
 # Destinations have a name, easy_location, hard_location
 destinations = []
 
-DRONE_URI = 'radio://0/80/2M/E7E7E7E7E0'
+DRONE_URI = "radio://0/80/2M/E7E7E7E7E0"
 FLIGHT_ZONE = FlightZone(2.0, 3.0, 1.25, 0.3)
 
 SCORE = 0
@@ -38,19 +37,21 @@ BASE_MOVEMENT_DISTANCE = 0.10
 MOVEMENT_RANGE = (0.0, 0.40)
 MOVEMENT_STEPS = 0.05
 
-GOAL_MARGIN = 0.5  # radius (in m) around a goal considered "valid"
+GOAL_MARGIN = 0.25  # radius (in m) around a goal considered "valid"
 
-DATA_FOLDER = Path('./data')
-PARTICIPANT_DATA_FOLDER = DATA_FOLDER / 'performance-data'
-LOG_FOLDER = DATA_FOLDER / 'logs'
+DATA_FOLDER = Path("./data")
+PARTICIPANT_DATA_FOLDER = DATA_FOLDER / "performance-data"
+LOG_FOLDER = DATA_FOLDER / "logs"
 
-DATA_FIELDNAMES = ['Participant ID',
-                   'Condition',
-                   'Trial',
-                   'Score',
-                   'Avg. time per action',
-                   'Time to complete trial',
-                   'Closest goal']
+DATA_FIELDNAMES = [
+    "Participant ID",
+    "Condition",
+    "Trial",
+    "Score",
+    "Avg. time per action",
+    "Time to complete trial",
+    "Closest goal",
+]
 
 
 def move_home(cf):
@@ -69,10 +70,8 @@ def move_home(cf):
     cf.swarm_land()
 
 
-def send_message(sock, action, data, data_type='na'):
-    sock.send(json.dumps({'action': action,
-                          'type': data_type,
-                          'data': data}))
+def send_message(sock, action, data, data_type="na"):
+    sock.send(json.dumps({"action": action, "type": data_type, "data": data}))
 
 
 def recieve_message(sock):
@@ -81,7 +80,7 @@ def recieve_message(sock):
     if isinstance(data, str):
         json_data = json.loads(data)
 
-        if 'action' in json_data:
+        if "action" in json_data:
             return json_data
         else:
             raise Exception("Recieved data is not in expected format!")
@@ -90,17 +89,13 @@ def recieve_message(sock):
 
 
 def start_trial_timer(sock):
-    send_message(sock,
-                 action='timer',
-                 data='start')
+    send_message(sock, action="timer", data="start")
 
     return time.time()
 
 
 def stop_trial_timer(sock, start_time):
-    send_message(sock,
-                 action='timer',
-                 data='stop')
+    send_message(sock, action="timer", data="stop")
 
     return time.time() - start_time
 
@@ -110,41 +105,39 @@ def update_score(sock, score_update):
 
     SCORE += score_update
 
-    send_message(sock,
-                 action='score',
-                 data=SCORE)
+    send_message(sock, action="score", data=SCORE)
 
 
-def write_row_to_csv(experiment_trial,
-                     trial_time,
-                     time_per_action,
-                     closest_goal):
-    row = {'Participant ID': app.config['id'],
-           'Condition': app.config['condition'],
-           'Trial': experiment_trial,
-           'Score': SCORE,
-           'Avg. time per action': time_per_action,
-           'Time to complete trial': trial_time,
-           'Closest goal': closest_goal}
+def write_row_to_csv(experiment_trial, trial_time, time_per_action, closest_goal):
+    row = {
+        "Participant ID": app.config["id"],
+        "Condition": app.config["condition"],
+        "Trial": experiment_trial,
+        "Score": SCORE,
+        "Avg. time per action": time_per_action,
+        "Time to complete trial": trial_time,
+        "Closest goal": closest_goal,
+    }
 
-    participant_file = (PARTICIPANT_DATA_FOLDER /
-        app.config['id']).with_suffix('.csv')
+    participant_file = (PARTICIPANT_DATA_FOLDER / app.config["id"]).with_suffix(".csv")
 
-    with participant_file.open(mode='a', newline='') as csvfile:
+    with participant_file.open(mode="a", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=DATA_FIELDNAMES)
         writer.writerow(row)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@sock.route('/action')
+@sock.route("/action")
 def echo(sock):
-    send_message(sock,
-                 action='alert',
-                 data='Welcome! This is your first flight.')
+    send_message(
+        sock,
+        action="alert",
+        data='Welcome! This is your first flight. Your time will start when you take off. Please press "Take Off" when you are ready',
+    )
 
     cf = SimulatedController({DRONE_URI}, FLIGHT_ZONE, DRONE_URI)
 
@@ -152,30 +145,33 @@ def echo(sock):
     action_times = []
 
     while experiment_trial < NUM_TRIALS:
-
         data = recieve_message(sock)
 
-        action = data['action']
+        action = data["action"]
+        trial_start = 0.0
+        action_timer_start = 0.0
 
         match action:
-            case 'out of time':
+            case "out of time":
                 trial_time = stop_trial_timer(sock, trial_start)
 
                 score_update = -BASE_SCORE
 
                 update_score(sock, score_update)
 
-                message = "You've run out of time, you've lost {} points! Moving drone back to home.".format(
-                    abs(score_update))
+                message = "You have run out of time, you've lost {} points! Moving drone back to home.".format(
+                    abs(score_update)
+                )
 
-                send_message(sock, 'alert', message)
+                send_message(sock, "alert", message)
 
                 closest_goal = (math.inf, None)
 
                 for row in destinations:
                     for goal in row:
-                        distance_to_current_goal = cf.distance_to_2D_point(DRONE_URI,
-                                                                           goal.position)
+                        distance_to_current_goal = cf.distance_to_2D_point(
+                            DRONE_URI, goal.position
+                        )
 
                         distance_to_old_goal = closest_goal[0]
 
@@ -187,10 +183,12 @@ def echo(sock):
                 if action_times:
                     avg_time_per_action = statistics.fmean(action_times)
 
-                write_row_to_csv(experiment_trial,
-                                 trial_time,
-                                 avg_time_per_action,
-                                 closest_goal[1].label)
+                write_row_to_csv(
+                    experiment_trial,
+                    trial_time,
+                    avg_time_per_action,
+                    closest_goal[1].label,
+                )
 
                 move_home(cf)
                 experiment_trial += 1
@@ -198,60 +196,63 @@ def echo(sock):
 
                 continue
 
-            case 'move':
+            case "move":
                 if cf.swarm_flying:
                     action_timer_stop = time.time() - action_timer_start
                     action_times.append(action_timer_stop)
 
                     action_timer_start = time.time()
 
-                    direction = data['direction']
+                    direction = data["direction"]
 
                     movement_distance = BASE_MOVEMENT_DISTANCE
 
-                    if app.config['condition'] == 'manipulation':
-                        movement_distance = random.randrange(int(MOVEMENT_RANGE[0] * 100),
-                                                             int(MOVEMENT_RANGE[1]
-                                                             * 100),
-                                                             int(MOVEMENT_STEPS * 100)) / 100
+                    if app.config["condition"] == "manipulation":
+                        movement_distance = (
+                            random.randrange(
+                                int(MOVEMENT_RANGE[0] * 100),
+                                int(MOVEMENT_RANGE[1] * 100),
+                                int(MOVEMENT_STEPS * 100),
+                            )
+                            / 100
+                        )
 
                     logger.info("Moving {}".format(direction))
 
                     movement = None
 
                     match direction:
-                        case 'forward':
+                        case "forward":
                             movement = [movement_distance, 0, 0]
-                        case 'back':
+                        case "back":
                             movement = [-movement_distance, 0, 0]
-                        case 'left':
+                        case "left":
                             movement = [0, movement_distance, 0]
-                        case 'right':
+                        case "right":
                             movement = [0, -movement_distance, 0]
                         case _:
-                            raise RuntimeError(
-                                "Unknown direction: " + direction)
+                            raise RuntimeError("Unknown direction: " + direction)
 
-                    cf.swarm_move({DRONE_URI: movement}, None, 2., True)
+                    cf.swarm_move({DRONE_URI: movement}, None, 2.0, True)
                 else:
-                    send_message(sock,
-                                 action='alert',
-                                 data_type='no takeoff',
-                                 data='Please take off before attempting to move!')
+                    send_message(
+                        sock,
+                        action="alert",
+                        data_type="no takeoff",
+                        data="Please take off before attempting to move!",
+                    )
 
-            case 'take off':
+            case "take off":
                 if cf.swarm_flying:
-                    raise RuntimeError(
-                        "Recieved take off command when already flying!")
+                    raise RuntimeError("Recieved take off command when already flying!")
 
                 trial_start = start_trial_timer(sock)
                 action_timer_start = time.time()
                 cf.swarm_take_off()
 
-            case 'land':
+            case "land":
                 if not cf.swarm_flying:
-                    raise RuntimeError(
-                        "Recieved land command when not flying!")
+                    raise RuntimeError("Recieved land command when not flying!")
 
                 trial_time = stop_trial_timer(sock, trial_start)
 
@@ -268,18 +269,22 @@ def echo(sock):
                     for row in destinations:
                         for goal in row:
                             distance_to_current_goal = cf.distance_to_2D_point(
-                                DRONE_URI, goal.position)
+                                DRONE_URI, goal.position
+                            )
 
                             drone_in_goal = distance_to_current_goal < GOAL_MARGIN
 
                             if drone_in_goal:
-                                closest_goal = (distance_to_current_goal,
-                                                goal)
+                                closest_goal = (distance_to_current_goal, goal)
                                 new_score = BASE_SCORE * goal.difficulty_modifier
 
-                                send_message(sock,
-                                             action='alert',
-                                             data="You've gained {} points! Moving drone back to home.".format(new_score))
+                                send_message(
+                                    sock,
+                                    action="alert",
+                                    data="You have gained {} points! Moving drone back to home.".format(
+                                        new_score
+                                    ),
+                                )
 
                                 # no need to continue searching
                                 # when we've found the closest goal
@@ -289,24 +294,29 @@ def echo(sock):
                             distance_to_old_goal = closest_goal[0]
 
                             if distance_to_current_goal < distance_to_old_goal:
-                                closest_goal = (distance_to_current_goal,
-                                                goal)
+                                closest_goal = (distance_to_current_goal, goal)
 
                 except StopIteration:
                     pass
 
                 if new_score < 0:
-                    send_message(sock,
-                                 action='alert',
-                                 data="You've lost {} points! Moving drone back to home.".format(abs(new_score)))
+                    send_message(
+                        sock,
+                        action="alert",
+                        data="You have lost {} points! Moving drone back to home.".format(
+                            abs(new_score)
+                        ),
+                    )
 
                 update_score(sock, new_score)
 
                 avg_time_per_action = statistics.fmean(action_times)
-                write_row_to_csv(experiment_trial,
-                                 trial_time,
-                                 avg_time_per_action,
-                                 closest_goal[1].label)
+                write_row_to_csv(
+                    experiment_trial,
+                    trial_time,
+                    avg_time_per_action,
+                    closest_goal[1].label,
+                )
 
                 move_home(cf)
                 action_times = []
@@ -315,49 +325,54 @@ def echo(sock):
             case _:
                 raise RuntimeError("Illegal action: " + action)
 
-    send_message(sock,
-                 action='alert',
-                 data='Destination reached! Well done! Going back to homebase.')
+    send_message(
+        sock,
+        action="alert",
+        data="You are done! Let the experiment leader know you are ready for the final questionnaire and debriefing.",
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Runs the risky drones experiment")
 
-    parser = argparse.ArgumentParser(
-        description="Runs the risky drones experiment")
+    parser.add_argument(
+        "-c",
+        "--condition",
+        dest="condition",
+        required=True,
+        choices=["control", "manipulation"],
+        help="Which experimental condition to run",
+    )
 
-    parser.add_argument('-c',
-                        '--condition',
-                        dest='condition',
-                        required=True,
-                        choices=['control', 'manipulation'],
-                        help='Which experimental condition to run')
+    parser.add_argument(
+        "-i", "--id", required=True, help="The id of the current participant"
+    )
 
-    parser.add_argument('-i',
-                        '--id',
-                        required=True,
-                        help='The id of the current participant')
-
-    parser.add_argument('-g',
-                        '--goal-file',
-                        dest='goalFile',
-                        type=Path,
-                        default='goals.bin',
-                        help='The generated file with goals to use')
+    parser.add_argument(
+        "-g",
+        "--goal-file",
+        dest="goalFile",
+        type=Path,
+        default="goals.bin",
+        help="The generated file with goals to use",
+    )
 
     args = parser.parse_args()
 
     PARTICIPANT_DATA_FOLDER.mkdir(parents=True, exist_ok=True)
     LOG_FOLDER.mkdir(parents=True, exist_ok=True)
 
-    log_file = (LOG_FOLDER / args.id).with_suffix('.csv')
+    log_file = (LOG_FOLDER / args.id).with_suffix(".log")
 
-    logging.basicConfig(format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                        datefmt='%H:%M:%S',
-                        level=logging.INFO,
-                        handlers=[logging.FileHandler(log_file), logging.StreamHandler(sys.stdout)])
+    logging.basicConfig(
+        format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG,
+        handlers=[logging.FileHandler(log_file), logging.StreamHandler(sys.stdout)],
+    )
 
-    app.config['condition'] = args.condition
-    app.config['id'] = args.id
+    app.config["condition"] = args.condition
+    app.config["id"] = args.id
 
     try:
         destinations = read_from_file(args.goalFile)
@@ -365,9 +380,9 @@ if __name__ == '__main__':
         logger.info("Could not find goal file {}".format(args.goalFile))
         quit()
 
-    participant_file = (PARTICIPANT_DATA_FOLDER / args.id).with_suffix('.csv')
+    participant_file = (PARTICIPANT_DATA_FOLDER / args.id).with_suffix(".csv")
 
-    with participant_file.open(mode='w', newline='') as csvfile:
+    with participant_file.open(mode="w", newline="") as csvfile:
         logger.info("Creating new participant file {}".format(participant_file))
         writer = csv.DictWriter(csvfile, fieldnames=DATA_FIELDNAMES)
         writer.writeheader()
